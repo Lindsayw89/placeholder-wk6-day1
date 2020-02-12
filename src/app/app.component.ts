4078eccd4094b1e945a28ccd0827e37ab7ef70db
lindsayw89 <lindsay.89@outlook.com>
2020-02-11 19:09:58 -0600

    table showing up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { PlaceholderService } from './services/placeholder.service';
+import {MatTableDataSource } from '@angular/material';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,8 +10,18 @@ import { PlaceholderService } from './services/placeholder.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private placeholderService : PlaceholderService){}
 
-  title = 'PlaceHolder';
+
+  constructor(private placeholderService : PlaceholderService){}
+  displayedColumns: string[] = ['userId', 'id', 'title', 'completed'];
+  dataSource;
+
+async ngOnInit() {
+this.dataSource= await this.placeholderService.get();
 }
+
+
+}
+
+
 
